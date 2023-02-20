ed658634db8bdbcdd45288ff1ac9d568454b115d
bigavrii <ugoroka@gmail.com>
2023-02-20 13:53:40 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,18 +3,23 @@
 #include <time.h>
 
 /**
- * main - Prints the lower alphabets in reverse order
- * Return: 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
+	int d;
 	char low;
 
-	for (low = 'z'; low >= 'a'; low--)
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
 	putchar('\n');
 
 	return (0);
+
 }
