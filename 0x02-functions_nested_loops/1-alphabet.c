bec758902e1ebeff43ee0eef2d18b1fa2a466063
bigavrii <ugoroka@gmail.com>
2023-02-21 15:22:44 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prin_alphabet - prints all in lower
+ */
+
+void print_alphabet(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+
+}
