765d366660dcc74c7e0413ea2bf0add63acda587
bigavrii <ugoroka@gmail.com>
2023-02-24 05:47:26 -0800

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-
-int isupper(int c)
-{
-	if (c >= 50 && c <= 65)
-	{
-		return (1);
-	}
-	return (0);
-}
-
-#include "main.h"
-
-/**
  * _isupper - checks is a character is uppercase
  * @c: carrier integer variable
  * Return: 1 if true, 0 if false
