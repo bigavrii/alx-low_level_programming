08480020d8151a549f73d679eb3dbc3ed6d8f79d
bigavrii <ugoroka@gmail.com>
2023-02-28 15:03:33 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @str: Pointer to the output
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+
+{
+	printf ("%s\n", str);
+}
