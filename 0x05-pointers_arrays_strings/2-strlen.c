592699d20b4d3a083e4e5300052f6f37b5a19a39
bigavrii <ugoroka@gmail.com>
2023-02-28 14:40:30 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * @program to return string
+ *
+ * Return: 0 Always
+ */
+
+int _strlen(char *s)
+
+{
+	int lenght = strlen(s);
+
+	return (lenght);
+
+}
