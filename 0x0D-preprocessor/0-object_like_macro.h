655f649e94ed1e024d5123b960094a1c8a41674c
bigavrii <ugoroka@gmail.com>
2023-03-21 19:32:46 -0700

    Task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,9 +1,10 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
-/**
- * File: Object_like_macro
- * Desc: file that defines a macro named SIZE as an abbreviation for the token 1024.
+/*
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named SIZE as an abbreviation for the token 1024.
+ *
  */
 
 #define SIZE 1024
