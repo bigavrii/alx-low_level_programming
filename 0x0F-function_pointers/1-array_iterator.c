1f184589d68f535b84c6eb58b67f981f41635bf3
bigavrii <ugoroka@gmail.com>
2023-03-23 16:58:24 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,13 +12,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array && action)
-    {
-	    size_t i;
-        for (i = 0; i < size; i++)
-	{
-            action(array[i]);
-        }
-    }
-}
+	unsigned int i;
 
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
