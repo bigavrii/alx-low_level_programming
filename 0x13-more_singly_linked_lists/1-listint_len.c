a864ce7eaa7f328fa6c546cb2402241e86596a52
bigavrii <ugoroka@gmail.com>
2023-04-04 14:19:21 -0700

    Task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t node = 0;
 
 	while(h != NULL)
 	{
 		h = h->next;
-		count++;
+		node++;
 	}
 
-	return (count);
+	return (node);
 }
